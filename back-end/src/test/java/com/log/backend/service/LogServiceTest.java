a3357b6cebeecb20121bf2741904174990fb2219
eltonhp <elton.hadriel@gmail.com>
2020-04-23 16:50:52 -0300

    feat: junit

--- back-end/src/test/java/com/log/backend/service/LogServiceTest.java
+++ back-end/src/test/java/com/log/backend/service/LogServiceTest.java
@@ -4,20 +4,11 @@ import com.log.backend.model.Log;
 import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.ResponseEntity;
-
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.client.RestTemplate;
-
 import java.net.URI;
 import java.net.URISyntaxException;
 
